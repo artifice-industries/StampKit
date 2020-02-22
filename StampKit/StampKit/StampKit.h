91012fa0dfa67e77b09ee5b07e488a803bf4a1bd
Sam Smallman <sammy@artificers.co.uk>
2020-02-22 09:06:52 +0000

    Add Xcode project

---
+++ StampKit/StampKit/StampKit.h
@@ -0,0 +1,19 @@
+//
+//  StampKit.h
+//  StampKit
+//
+//  Created by Sam Smallman on 22/02/2020.
+//  Copyright © 2020 Artifice Industries Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StampKit.
+FOUNDATION_EXPORT double StampKitVersionNumber;
+
+//! Project version string for StampKit.
+FOUNDATION_EXPORT const unsigned char StampKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StampKit/PublicHeader.h>
+
+
