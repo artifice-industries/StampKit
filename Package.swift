1002581c29dabda8166f0f59a55156b1627f049b
Sam Smallman <sammy@artificers.co.uk>
2020-05-13 09:23:31 +0100

    Update OSC branch dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SammyTheHand/OSCKit", .branch("OSCK-#15"))
+        .package(url: "https://github.com/SammyTheHand/OSCKit", .branch("develop"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
